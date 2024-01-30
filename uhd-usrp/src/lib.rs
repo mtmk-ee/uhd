2ac6d4ad7643da37085dc55cc95d96ee063f7366
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2024-01-30 01:59:03 -0500

    Add driver version getter

--- uhd-usrp/src/lib.rs
+++ uhd-usrp/src/lib.rs
@@ -1,7 +1,15 @@
-
 mod error;
+pub(crate) mod misc_types;
 pub mod usrp;
 pub(crate) mod util;
-pub(crate) mod misc_types;
 
-pub use error::{Result, UhdError};
+pub use error::{last_error_message, Result, UhdError};
+
+use crate::error::try_uhd;
+
+pub fn driver_version() -> Result<String> {
+    const BUFF_LEN: usize = 16;
+    let mut buff = [0u8; BUFF_LEN];
+    try_uhd!(unsafe { uhd_usrp_sys::uhd_get_version_string(buff.as_mut_ptr().cast(), BUFF_LEN) })?;
+    String::from_utf8(buff.to_vec()).or(Err(UhdError::Unknown))
+}
