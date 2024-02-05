cfc4d9dc7450e2d36b0a94e44527301ccc14370b
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2024-02-05 02:36:02 -0500

    Add more sample types

--- uhd-usrp/src/sample.rs
+++ uhd-usrp/src/sample.rs
@@ -9,18 +9,45 @@ impl Sample for Complex<f32> {
         "fc32"
     }
 }
+
 impl Sample for Complex<f64> {
     fn name() -> &'static str {
         "fc64"
     }
 }
+
 impl Sample for Complex<i8> {
     fn name() -> &'static str {
         "sc8"
     }
 }
+
 impl Sample for Complex<i16> {
     fn name() -> &'static str {
         "sc16"
+    }
+}
+
+impl Sample for f32 {
+    fn name() -> &'static str {
+        "f32"
+    }
+}
+
+impl Sample for f64 {
+    fn name() -> &'static str {
+        "f64"
+    }
+}
+
+impl Sample for i8 {
+    fn name() -> &'static str {
+        "s8"
+    }
+}
+
+impl Sample for i16 {
+    fn name() -> &'static str {
+        "s16"
     }
 }
