2d56f14adef6bc847e1fc84acf77dac79a40bbf3
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2024-02-05 02:51:26 -0500

    Add "num" feature flag

--- uhd-usrp/src/sample.rs
+++ uhd-usrp/src/sample.rs
@@ -1,28 +1,30 @@
-use num::Complex;
-
 pub trait Sample {
     fn name() -> &'static str;
 }
 
-impl Sample for Complex<f32> {
+#[cfg(feature = "num")]
+impl Sample for num_complex::Complex<f32> {
     fn name() -> &'static str {
         "fc32"
     }
 }
 
-impl Sample for Complex<f64> {
+#[cfg(feature = "num")]
+impl Sample for num_complex::Complex<f64> {
     fn name() -> &'static str {
         "fc64"
     }
 }
 
-impl Sample for Complex<i8> {
+#[cfg(feature = "num")]
+impl Sample for num_complex::Complex<i8> {
     fn name() -> &'static str {
         "sc8"
     }
 }
 
-impl Sample for Complex<i16> {
+#[cfg(feature = "num")]
+impl Sample for num_complex::Complex<i16> {
     fn name() -> &'static str {
         "sc16"
     }
