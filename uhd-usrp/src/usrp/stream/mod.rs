9ecfe773741eaf6eca05547a4e86e15080a935cd
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2024-03-25 16:15:37 -1000

    Streamer upgrade (#4)

--- uhd-usrp/src/usrp/stream/mod.rs
+++ uhd-usrp/src/usrp/stream/mod.rs
@@ -1,7 +1,26 @@
 mod rx_stream;
-mod stream_args;
 mod tx_stream;
 
-pub use rx_stream::{RxStream, RxStreamReader, RxStreamReaderOptions, RecvOptions};
-pub use stream_args::{CpuFormat, OtwFormat, StreamArgs, UnderflowPolicy};
-pub use tx_stream::TxStream;
+pub use rx_stream::{RxStream, RxStreamBuilder, RxStreamReader};
+pub use tx_stream::{TxStream, TxStreamBuilder, TxStreamWriter};
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum OtwFormat {
+    ComplexInt16,
+    ComplexInt12,
+    ComplexInt8,
+    Int16,
+    Int8,
+}
+
+impl OtwFormat {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            OtwFormat::ComplexInt16 => "sc16",
+            OtwFormat::ComplexInt12 => "sc12",
+            OtwFormat::ComplexInt8 => "sc8",
+            OtwFormat::Int16 => "s16",
+            OtwFormat::Int8 => "s8",
+        }
+    }
+}
--- uhd-usrp/src/usrp/stream/tx_stream.rs
+++ uhd-usrp/src/usrp/stream/tx_stream.rs
@@ -1,53 +1,137 @@
-use std::{cell::Cell, marker::PhantomData, ptr::addr_of_mut, time::Duration};
-
-use crate::{
-    error::try_uhd,
-    usrp::{metadata::TxMetadata, Usrp},
-    Result, Sample, UhdError,
+use std::{
+    cell::Cell, collections::HashMap, ffi::CString, marker::PhantomData, ptr::addr_of_mut,
+    time::Duration,
 };
 
-use super::StreamArgs;
+use super::OtwFormat;
+use crate::{
+    error::try_uhd,
+    ffi::OwnedHandle,
+    usrp::{metadata::TxMetadata, Usrp},
+    Result, Sample, SampleBuffer,
+};
 
-pub struct TxStream<T: Sample> {
-    handle: uhd_usrp_sys::uhd_tx_streamer_handle,
-    samples_per_buffer: usize,
-    channels: usize,
-    _unsync: PhantomData<Cell<T>>,
-    _ugh: PhantomData<T>,
+/// An owned handle for a USRP TX stream.
+pub(crate) type TxStreamHandle = OwnedHandle<uhd_usrp_sys::uhd_tx_streamer>;
+
+/// Arguments for creating a TX stream.
+pub struct TxStreamBuilder<'usrp, T>
+where
+    T: Sample,
+{
+    usrp: &'usrp Usrp,
+    otw_format: Option<OtwFormat>,
+    args: HashMap<String, String>,
+    channels: Vec<usize>,
+    _phantom: PhantomData<T>,
 }
 
-impl<T: Sample> TxStream<T> {
-    pub(crate) fn open(usrp: &Usrp, args: StreamArgs<T>) -> Result<Self> {
-        let mut handle: uhd_usrp_sys::uhd_tx_streamer_handle = std::ptr::null_mut();
-        let args = args.leak();
-        try_uhd!(unsafe { uhd_usrp_sys::uhd_tx_streamer_make(&mut handle) })?;
-        let res = try_uhd!(unsafe {
-            uhd_usrp_sys::uhd_usrp_get_tx_stream(
-                usrp.handle().as_mut_ptr(),
-                args.inner() as *const _ as *mut _,
-                handle,
-            )
-            .into()
-        });
-        match res {
-            Ok(()) => Self::with_handle(handle),
-            Err(e) => {
-                unsafe {
-                    uhd_usrp_sys::uhd_tx_streamer_free(&mut handle);
-                }
-                Err(e)
-            }
+impl<'usrp, T> TxStreamBuilder<'usrp, T>
+where
+    T: Sample,
+{
+    pub(crate) fn new(usrp: &'usrp Usrp) -> Self {
+        Self {
+            usrp,
+            otw_format: None,
+            args: HashMap::new(),
+            channels: vec![0],
+            _phantom: PhantomData::default(),
         }
     }
 
-    pub(crate) fn with_handle(handle: uhd_usrp_sys::uhd_tx_streamer_handle) -> Result<Self> {
+    /// Specify the "over the wire" format to use.
+    ///
+    /// If unspecified, a format will be chosen automatically.
+    pub fn with_otw_format(&mut self, format: OtwFormat) -> &mut Self {
+        self.otw_format = Some(format);
+        self
+    }
+
+    /// Specify which channels will be used for transmission.
+    ///
+    /// Defaults to a single channel, `0`.
+    pub fn with_channels(&mut self, channels: &[usize]) -> &mut Self {
+        self.channels = channels.to_vec();
+        self
+    }
+
+    /// Specify a keyword argument for the stream.
+    ///
+    /// This can be used to set other less common arguments.
+    /// The arguments are sent to UHD in the form `"arg=value"`.
+    ///
+    /// # Panics
+    ///
+    /// This will panic if either `arg` or `value` contains an `'='` character
+    /// or null byte.
+    pub fn with_kwarg(&mut self, arg: &str, value: &str) -> &mut Self {
+        assert!(!arg.contains('='), "argument cannot contain '='");
+        assert!(!value.contains('='), "value cannot contain '='");
+        assert!(!arg.contains('\0'), "argument cannot contain null bytes");
+        assert!(!value.contains('\0'), "value cannot contain null bytes");
+
+        self.args.insert(arg.to_string(), value.to_string());
+        self
+    }
+
+    /// Open the TX stream using the previously-specified arguments.
+    #[must_use]
+    pub fn open(&self) -> Result<TxStream<T>> {
+        let mut handle: uhd_usrp_sys::uhd_tx_streamer_handle = std::ptr::null_mut();
+        if let Err(e) = try_uhd!(unsafe { uhd_usrp_sys::uhd_tx_streamer_make(&mut handle) }) {
+            unsafe { uhd_usrp_sys::uhd_tx_streamer_free(addr_of_mut!(handle)) };
+            return Err(e);
+        }
+
+        let cpu_format = CString::new(T::name()).unwrap();
+        let otw_format = CString::new(self.otw_format.map(|f| f.as_str()).unwrap_or("")).unwrap();
+        let args = CString::new(
+            self.args
+                .iter()
+                .map(|(k, v)| format!("{k}={v}"))
+                .collect::<Vec<_>>()
+                .join(","),
+        )
+        .unwrap();
+        let mut stream_args = uhd_usrp_sys::uhd_stream_args_t {
+            cpu_format: cpu_format.as_ptr() as *mut _,
+            otw_format: otw_format.as_ptr() as *mut _,
+            args: args.as_ptr() as *mut _,
+            channel_list: self.channels.as_ptr().cast_mut(),
+            n_channels: self.channels.len() as i32,
+        };
+
+        try_uhd!(unsafe {
+            uhd_usrp_sys::uhd_usrp_get_tx_stream(
+                self.usrp.handle().as_mut_ptr(),
+                addr_of_mut!(stream_args),
+                handle,
+            )
+        })?;
+        TxStream::<T>::new(unsafe {
+            OwnedHandle::from_ptr(handle, uhd_usrp_sys::uhd_tx_streamer_free)
+        })
+    }
+}
+
+pub struct TxStream<T: Sample> {
+    handle: TxStreamHandle,
+    samples_per_buffer: usize,
+    channels: usize,
+
+    _unsync: PhantomData<Cell<T>>,
+}
+
+impl<T: Sample> TxStream<T> {
+    pub(crate) fn new(handle: TxStreamHandle) -> Result<Self> {
         let mut spb = 0;
         let mut channels = 0;
         try_uhd!(unsafe {
-            uhd_usrp_sys::uhd_tx_streamer_max_num_samps(handle, addr_of_mut!(spb))
+            uhd_usrp_sys::uhd_tx_streamer_max_num_samps(handle.as_mut_ptr(), addr_of_mut!(spb))
         })?;
         try_uhd!(unsafe {
-            uhd_usrp_sys::uhd_tx_streamer_num_channels(handle, addr_of_mut!(channels))
+            uhd_usrp_sys::uhd_tx_streamer_num_channels(handle.as_mut_ptr(), addr_of_mut!(channels))
         })?;
 
         Ok(Self {
@@ -55,59 +139,94 @@ impl<T: Sample> TxStream<T> {
             samples_per_buffer: spb,
             channels,
             _unsync: PhantomData::default(),
-            _ugh: PhantomData::default(),
         })
+    }
+
+    pub(crate) fn handle(&self) -> &TxStreamHandle {
+        &self.handle
+    }
+
+    pub fn max_samples_per_channel(&self) -> usize {
+        self.samples_per_buffer
     }
 
     pub fn channels(&self) -> usize {
         self.channels
     }
 
-    pub fn max_samples_per_buffer(&self) -> usize {
-        self.samples_per_buffer
+    pub fn writer(&mut self) -> TxStreamWriter<T> {
+        TxStreamWriter::new(self)
+    }
+}
+
+unsafe impl<T: Sample + Send> Send for TxStream<T> {}
+
+pub struct TxStreamWriter<'stream, 'md, T>
+where
+    T: Sample,
+{
+    stream: &'stream mut TxStream<T>,
+    timeout: Option<Duration>,
+    one_packet: bool,
+    metadata: Option<&'md mut TxMetadata>,
+}
+
+impl<'stream, 'md, T> TxStreamWriter<'stream, 'md, T>
+where
+    T: Sample,
+{
+    pub fn new(stream: &'stream mut TxStream<T>) -> Self {
+        Self {
+            stream,
+            timeout: None,
+            one_packet: false,
+            metadata: None,
+        }
     }
 
-    pub fn send(
+    pub fn with_one_packet(&mut self, one_packet: bool) -> &mut Self {
+        self.one_packet = one_packet;
+        self
+    }
+
+    pub fn with_timeout(&mut self, timeout: Duration) -> &mut Self {
+        self.timeout = Some(timeout);
+        self
+    }
+
+    pub fn with_metadata(&mut self, metadata: &'md mut TxMetadata) -> &mut Self {
+        self.metadata = Some(metadata);
+        self
+    }
+
+    pub fn send<B>(&mut self, buff: &B) -> Result<usize>
+    where
+        B: SampleBuffer<T>,
+    {
+        unsafe { self.send_raw(buff.as_ptr(), buff.samples()) }
+    }
+
+    pub unsafe fn send_raw(
         &mut self,
-        data: &[&[T]],
-        metadata: &TxMetadata,
-        timeout: Duration,
+        buff: *const *const T,
+        samples_per_channel: usize,
     ) -> Result<usize> {
-        if data.len() > 1 && data.iter().any(|e| e.len() != data[0].len()) {
-            return Err(UhdError::Index);
-        }
-        let mut buff = data.iter().map(|buff| buff.as_ptr()).collect::<Vec<_>>();
         let mut sent = 0;
-        let handle = metadata.to_handle()?;
+        let metadata_handle = self
+            .metadata
+            .as_ref()
+            .map(|md| md.to_handle())
+            .unwrap_or_else(|| TxMetadata::new().to_handle());
         try_uhd!(unsafe {
             uhd_usrp_sys::uhd_tx_streamer_send(
-                self.handle,
-                buff.as_mut_ptr().cast(),
-                data[0].len(),
-                handle.as_mut_mut_ptr(),
-                timeout.as_secs_f64(),
+                self.stream.handle().as_mut_ptr(),
+                buff.cast_mut().cast(),
+                samples_per_channel,
+                metadata_handle.as_mut_mut_ptr(),
+                self.timeout.unwrap_or_default().as_secs_f64(),
                 addr_of_mut!(sent),
             )
         })?;
         Ok(sent)
     }
-
-    pub fn send_one_channel(
-        &mut self,
-        data: &[T],
-        metadata: &TxMetadata,
-        timeout: Duration,
-    ) -> Result<usize> {
-        self.send(&[data], metadata, timeout)
-    }
 }
-
-impl<T: Sample> Drop for TxStream<T> {
-    fn drop(&mut self) {
-        unsafe {
-            uhd_usrp_sys::uhd_tx_streamer_free(addr_of_mut!(self.handle));
-        }
-    }
-}
-
-unsafe impl<T: Sample + Send> Send for TxStream<T> {}
