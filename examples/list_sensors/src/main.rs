885f847a0c4794f9fd9446422c8eb36b1d36c388
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2024-03-30 21:50:28 -1000

    Add list_sensors example (#6)

---
+++ examples/list_sensors/src/main.rs
@@ -0,0 +1,66 @@
+use std::error::Error;
+
+use clap::Parser;
+
+use uhd_usrp::Usrp;
+
+#[derive(clap::Parser)]
+struct Args {
+    #[arg(long)]
+    args: String,
+}
+
+enum Direction {
+    Rx,
+    Tx,
+}
+
+fn print_db_sensors(usrp: &Usrp, mb_idx: usize, dir: Direction) {
+    let sensors = match dir {
+        Direction::Rx => usrp.mboard(mb_idx).rx_subdev_spec().unwrap(),
+        Direction::Tx => usrp.mboard(mb_idx).tx_subdev_spec().unwrap(),
+    };
+    let n_chans = sensors.len();
+
+    for chan_idx in 0..n_chans {
+        match dir {
+            Direction::Rx => println!("* Rx Channel {chan_idx}:"),
+            Direction::Tx => println!("* Tx Channel {chan_idx}:"),
+        }
+        let sensors = match dir {
+            Direction::Rx => usrp.rx_config(chan_idx).sensor_names().unwrap(),
+            Direction::Tx => usrp.tx_config(chan_idx).sensor_names().unwrap(),
+        };
+        for sensor in sensors {
+            let sensor_value = match dir {
+                Direction::Rx => usrp.rx_config(chan_idx).sensor_value(&sensor).unwrap(),
+                Direction::Tx => usrp.tx_config(chan_idx).sensor_value(&sensor).unwrap(),
+            };
+            println!("\t* {} = {}", sensor, sensor_value.to_pp_string().unwrap());
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let usrp = Usrp::open_with_args(&args.args)?;
+
+    let n_mboards = usrp.n_mboards()?;
+    println!("Device contains {n_mboards} motherboard(s).");
+    for i in 0..n_mboards {
+        println!("_____________________________________________");
+        println!("Motherboard {i}:");
+
+        let mboard_sensors = usrp.mboard(i).sensor_names().unwrap();
+        for sensor in mboard_sensors {
+            let value = usrp.mboard(i).sensor_value(&sensor).unwrap();
+            println!("* {} = {}", sensor, value.to_pp_string().unwrap());
+        }
+
+        print_db_sensors(&usrp, i, Direction::Rx);
+        print_db_sensors(&usrp, i, Direction::Tx);
+    }
+
+    Ok(())
+}
